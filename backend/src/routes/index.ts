16b7ce45c5653d95f930ec673449f48f173d4804
maeva <maeva.arroniz@epitech.eu>
2026-09-02 15:14:08 +0200

    update: swagger and index.ts to list video routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import healthRoutes from './health';
 import { authRouter } from './auth';
 import { profileRouter } from './profile';
+import { videoRouter } from './video';
 
 const router = Router();
 
@@ -9,5 +10,6 @@ const router = Router();
 router.use('/health', healthRoutes);
 router.use('/auth', authRouter);
 router.use('/profile', profileRouter);
+router.use('/video', videoRouter);
 
 export default router;
