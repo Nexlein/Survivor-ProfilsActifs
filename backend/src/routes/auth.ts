2a53ecefad3f9a577999cb64a44957c66e698252
maeva <maeva.arroniz@epitech.eu>
2026-09-02 09:59:27 +0200

    update: auth routes with logout and refresh

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getCurrentUser, login, register } from '../controllers/auth';
+import { getCurrentUser, login, register, logout, refresh } from '../controllers/auth';
 
 export const authRouter = Router();
 
 authRouter.post('/login', login);
 authRouter.post('/register', register);
 authRouter.get('/get-current-user', getCurrentUser);
+authRouter.post('/logout', logout);
+authRouter.post('/refresh', refresh);
 
 export default authRouter;
